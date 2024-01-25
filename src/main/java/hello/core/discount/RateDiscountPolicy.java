4d1b1fb7eab25989d7e20aa0ad739ec720f0a7be
ksw4060 <gokorea1214@kakao.com>
2024-01-25 15:04:23 +0900

    의존성 주입시, 자동이냐 수동이냐?는 서비스 로직인가와 기술 로직이냐에 따라 선택하는 것이 일반적이다. 롬복을 이용해서, 생성자 주입 및 세터,게터까지 사용해보았다

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,11 +1,15 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 // 컴포넌트 스캔과 의존관계 자동 주입을 위해서, @Component, @Autowired 가 추가됨.
 @Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,21 +1,26 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 // 컴포넌트 스캔과 의존관계 자동 주입을 위해서, @Component, @Autowired 가 추가됨.
 @Component
+@Getter // 테스트 용도로 잠시 사용.
+//@RequiredArgsConstructor // 생성자 주입을 알아서 해주는, 롬복 어노테이션
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -27,10 +32,5 @@ public class OrderServiceImpl implements OrderService{
         int discountPrice = discountPolicy.discount(member, itemPrive);
 
         return new Order(memberId, itemName, itemPrive, discountPrice);
-    }
-
-    // 테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
     }
 }
